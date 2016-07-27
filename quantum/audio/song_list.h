905800f0ca72eaa77f2cea65f3d4274113ac1ac2
Johan Sjolin <johan.sjolin@maginteractive.se>
2016-07-27 18:18:11 +0200

    started adding zelda audio

--- quantum/audio/song_list.h
+++ quantum/audio/song_list.h
@@ -3,6 +3,13 @@
 #ifndef SONG_LIST_H
 #define SONG_LIST_H
 
+
+#define ZELDA_SECRET                                        \
+    Q__NOTE(_E4), Q__NOTE(_E4), Q__NOTE(_F4), Q__NOTE(_G4), \
+    Q__NOTE(_G4), Q__NOTE(_F4), Q__NOTE(_E4), Q__NOTE(_D4), \
+    Q__NOTE(_C4), Q__NOTE(_C4), Q__NOTE(_D4), Q__NOTE(_E4), \
+    QD_NOTE(_E4), E__NOTE(_D4), H__NOTE(_D4),
+
 #define ODE_TO_JOY                                          \
     Q__NOTE(_E4), Q__NOTE(_E4), Q__NOTE(_F4), Q__NOTE(_G4), \
     Q__NOTE(_G4), Q__NOTE(_F4), Q__NOTE(_E4), Q__NOTE(_D4), \
